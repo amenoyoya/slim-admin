2e4be1472ee2f296dd6b4d026480200180d4fcde
amenoyoya <mtyswe3@gmail.com>
2019-09-07 22:27:19 +0900

    fix: nav-bar: ログイン中｜ログアウト中で表示切り替え

--- www/src/components/parts/NavBar.vue
+++ www/src/components/parts/NavBar.vue
@@ -2,21 +2,31 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item has-text-info is-size-4" href="https://github.com/amenoyoya">
-        <i class="fas fa-atom"></i> AM3:ware
+        <i class="fas fa-atom"></i> &nbsp; AM3:ware
       </a>
     </div>
 
     <div class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item" @click.prevent="toHome">
-          <i class="fas fa-home"></i> Home
-        </a>
+        <router-link to="/dashboard" class="navbar-item">
+          <i class="fas fa-home"></i> &nbsp; Home
+        </router-link>
       </div>
 
       <div class="navbar-end">
-        <div class="navbar-item">
+        <!-- ユーザー名｜ログアウト -->
+        <div class="navbar-item has-dropdown is-hoverable" v-if="$store.state.auth.username !== ''">
+          <a class="navbar-link">
+            <i class="fas fa-user"></i> &nbsp; {{ $store.state.auth.username }}
+          </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">Log out</a>
+          </div>
+        </div>
+        <!-- サインイン｜ログイン -->
+        <div class="navbar-item" v-else>
           <div class="buttons">
-            <a class="button is-danger">
+            <a class="button is-danger" disabled="true">
               <strong>Sign up</strong>
             </a>
             <a class="button is-light">
@@ -28,13 +38,3 @@
     </div>
   </nav>
 </template>
-
-<script>
-export default {
-  methods: {
-    toHome() {
-      self.$route.push('/');
-    }
-  }
-};
-</script>
